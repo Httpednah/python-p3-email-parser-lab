5b120a58a9b8950da9a70ba21f29455426159ed1
Ednah wairimu kimani <kimaniednah@gmail.com>
2025-11-29 10:41:46 +0300

    lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        # Find all valid email addresses in the string
+        emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}", self.email_addresses)
+        # Sort alphabetically
+        return sorted(emails)
